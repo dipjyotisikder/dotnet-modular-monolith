c3e628136f698ec465f547b7db1f9ab40b6c792d
Dipjyoti Sikder <dipjyotisikder@gmail.com>
2026-03-22 15:01:56 +0600

    Added auditable properties for users table

---
+++ src/Modules/Users/Users.Infrastructure/Persistence/Migrations/20260322083859_AddedAuditableProperties.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Users.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedAuditableProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TokenRevocationVersion",
+                schema: "Users",
+                table: "Users",
+                type: "text",
+                nullable: true,
+                defaultValue: "{}",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldDefaultValue: "{}");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatedBy",
+                schema: "Users",
+                table: "Users",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                schema: "Users",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ModifiedBy",
+                schema: "Users",
+                table: "Users",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                schema: "Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                schema: "Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                schema: "Users",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TokenRevocationVersion",
+                schema: "Users",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "{}",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true,
+                oldDefaultValue: "{}");
+        }
+    }
+}
