3bdf72f70d6797105ab86b0b56c902811b0afaa0
Dipjyoti Sikder <dipjyotisikder@gmail.com>
2026-03-22 15:01:38 +0600

    Added auditable properties in bookings table

--- src/Modules/Bookings/Bookings.Infrastructure/Persistence/Migrations/20260322083953_AddedAuditableProperties.Designer.cs
+++ src/Modules/Bookings/Bookings.Infrastructure/Persistence/Migrations/20260322083953_AddedAuditableProperties.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Bookings.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Bookings.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(BookingsDbContext))]
-    partial class BookingsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260322083953_AddedAuditableProperties")]
+    partial class AddedAuditableProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Modules/Bookings/Bookings.Infrastructure/Persistence/Migrations/20260322083953_AddedAuditableProperties.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookings.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedAuditableProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatedBy",
+                schema: "Bookings",
+                table: "Bookings",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                schema: "Bookings",
+                table: "Bookings",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ModifiedBy",
+                schema: "Bookings",
+                table: "Bookings",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                schema: "Bookings",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                schema: "Bookings",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                schema: "Bookings",
+                table: "Bookings");
+        }
+    }
+}
